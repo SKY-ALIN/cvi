47856984edc48b216a7a3e66aa185b6d81b5feb7
Vladimir Alinsky <adam3iks@gmail.com>
2020-03-30 02:46:20 +0700

    Скрипт для получения списка доступных камер

--- cameras_list.py
+++ cameras_list.py
@@ -1,6 +1,8 @@
+"""Скрипт выводит список id доступных камер."""
+
 import cv2
 arr = []
-for i in range(20):
+for i in range(30):
     cap = cv2.VideoCapture(i)
     if cap.read()[0]:
         arr.append(i)
